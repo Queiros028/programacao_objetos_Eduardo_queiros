bc3f2b4d1810c6fbba5781b895b36a8a9e14e0f2
Queiros028 <wadosecsgo@gmail.com>
2023-11-15 19:26:06 +0000

    15-11

---
+++ 23005_Pratica_POO_IM_Fase1/Pessoa/Pessoa.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum DescricaoPessoa
+{
+    UTENTE,
+    FUNCIONARIO,
+    MEDICO
+}
+
+namespace clinicaDentario_23005
+{
+     public class Pessoa
+    {
+        #region Atributos
+        public string nome;
+        public DescricaoPessoa descricao;
+        static int cod; //esta variavel vai ser a que vai identificar cada pessoa na clinica
+        public int codGlobal;
+
+        #endregion
+
+        #region Métodos
+
+        #region Construtores
+        /// <summary>
+        /// Construtor de classe: Inicializa variaveis
+        /// </summary>
+        static Pessoa()
+        {
+            cod = 1;
+        }
+
+        public Pessoa()
+        {
+            nome = "";
+            descricao = tipo;
+            codGlobal = cod;
+            cod++;
+        }
+
+        #endregion
+
+        #region Propriedades
+        public string Nome
+        {
+            set { nome = value; }
+            get { return nome; }
+        }
+
+        public DescricaoPessoa tipo
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+        #endregion
+
+        #region override
+        public override string ToString()
+        {
+            return String.Format("Ficha da Pessoa-> Nome: {0}; Funcao: {1} ; codigo {2}\n", nome, descricao, codGlobal);
+        }
+
+        #endregion
+
+
+        #region Outros Metodos
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ 23005_Pratica_POO_IM_Fase1/clinicaDentario_23005/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+using consulta;
+using Pessoa;
+*/
+namespace clinicaDentario_23005
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+            Pessoa p3 = new Pessoa();
+
+            Console.WriteLine("" + p1.ToString());
+            Console.WriteLine("" + p2.ToString());
+            Console.WriteLine("" + p3.ToString());
+
+            Console.ReadKey();  
+        }
+    }
+}
